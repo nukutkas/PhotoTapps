9cfb4e3c31022071dd29f8bffc783fa28bd66f2f
Nikita Kochetov <nothsmple@mail.ru>
2021-05-14 09:44:33 +0300

    Deleted unnecessary methods

--- PhotoTapps/PhotoViewController.swift
+++ PhotoTapps/PhotoViewController.swift
@@ -12,11 +12,13 @@ class PhotoViewController: UIViewController {
     
     var image: UIImage?
     @IBOutlet weak var photoImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         photoImageView.image = image
     }
+    
     @IBAction func shareAction(_ sender: Any) {
         
         let shareController = UIActivityViewController(activityItems: [image!], applicationActivities: nil)
@@ -30,16 +32,4 @@ class PhotoViewController: UIViewController {
         
         present(shareController, animated: true, completion: nil)
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
--- PhotoTapps/PhotosCollectionViewController.swift
+++ PhotoTapps/PhotosCollectionViewController.swift
@@ -17,18 +17,6 @@ class PhotosCollectionViewController: UICollectionViewController {
     let itemsPerRow: CGFloat = 2
     let sectionInsets = UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20)
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-//        let layout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout
-//        layout.itemSize = CGSize(width: 70, height: 30)
-//        layout.sectionInset = UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20)
-//        layout.minimumLineSpacing = 1
-//        layout.minimumInteritemSpacing = 1
-//        layout.scrollDirection = .vertical
-//
-//        collectionView.showsVerticalScrollIndicator = false
-
-    }
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "pickPhotoSegue" {
             let photoVC = segue.destination as! PhotoViewController
@@ -83,6 +71,4 @@ extension PhotosCollectionViewController: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
         return sectionInsets.left
     }
-
-
 }
